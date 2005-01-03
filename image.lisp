446cbc0a0ea0dc8e0e370c886e15044ac4e2a541
Matthieu Villeneuve <matthieu.villeneuve@free.fr>
2005-01-03 20:45:41 +0000

    Use correct pixel types in declarations

--- image.lisp
+++ image.lisp
@@ -12,6 +12,7 @@
 
 (in-package :imago)
 
+
 (defclass image ()
   ()
   (:documentation "The protocol class for images."))
@@ -22,28 +23,29 @@
 (defgeneric image-width (image)
   (:documentation "Returns the width of the image."))
 
-(defgeneric pixel-size (image)
-  (:documentation "Returns the number of bytes used to represent a pixel."))
-
-(defmethod image-width ((image image))
-  (second (array-dimensions (image-pixels image))))
-
 (defgeneric image-height (image)
   (:documentation "Returns the height of the image."))
-
-(defmethod image-height ((image image))
-  (first (array-dimensions (image-pixels image))))
 
 (defgeneric image-pixel (image x y)
   (:documentation "Returns the color of the pixel at specified coordinates
 in the image."))
 
-(defmethod image-pixel ((image image) x y)
-  (aref (image-pixels image) y x))
-
 (defgeneric (setf image-pixel) (pixel image x y)
   (:documentation "Sets the color of the pixel at specified coordinates
 in the image."))
+
+(defgeneric pixel-size (image)
+  (:documentation "Returns the number of bytes used to represent a pixel."))
+
+
+(defmethod image-width ((image image))
+  (second (array-dimensions (image-pixels image))))
+
+(defmethod image-height ((image image))
+  (first (array-dimensions (image-pixels image))))
+
+(defmethod image-pixel ((image image) x y)
+  (aref (image-pixels image) y x))
 
 (defmethod (setf image-pixel) (pixel (image image) x y)
   (setf (aref (image-pixels image) y x) pixel))
@@ -52,8 +54,9 @@ in the image."))
   (print-unreadable-object (object stream :type t :identity t)
     (format stream "(~Dx~D)" (image-width object) (image-height object))))
 
+
 (defclass rgb-image (image)
-  ((pixels :type (simple-array (unsigned-byte 32) (* *))
+  ((pixels :type (simple-array rgb-pixel (* *))
            :reader image-pixels))
   (:documentation "The class for RGB images. Image dimensions must be
 provided to MAKE-INSTANCE, through the :WIDTH and :HEIGHT keyword
@@ -66,13 +69,15 @@ parameters."))
          (setf (slot-value image 'pixels) pixels))
         ((and (numberp width) (numberp height))
          (setf (slot-value image 'pixels)
-               (make-array (list height width) :initial-element 0)))
+               (make-array (list height width)
+                           :element-type 'rgb-pixel)))
         (t (error "Invalid initialization arguments"))))
 
 (defmethod pixel-size ((image rgb-image)) 4)
 
+
 (defclass grayscale-image (image)
-  ((pixels :type (simple-array (unsigned-byte 8) (* *))
+  ((pixels :type (simple-array grayscale-pixel (* *))
            :reader image-pixels))
   (:documentation "The class for grayscale images. Image dimensions must be
 provided to MAKE-INSTANCE, through the :WIDTH and :HEIGHT keyword
@@ -85,13 +90,15 @@ parameters."))
          (setf (slot-value image 'pixels) pixels))
         ((and (numberp width) (numberp height))
          (setf (slot-value image 'pixels)
-               (make-array (list height width) :initial-element 0)))
+               (make-array (list height width)
+                           :element-type 'grayscale-pixel)))
         (t (error "Invalid initialization arguments"))))
 
 (defmethod pixel-size ((image grayscale-image)) 2)
 
+
 (defclass indexed-image (image)
-  ((pixels :type (simple-array unsigned-byte (* *))
+  ((pixels :type (simple-array indexed-pixel (* *))
            :reader image-pixels)
    (colormap :initarg :colormap :reader image-colormap))
   (:documentation "The class for indexed images. Image dimensions must be
@@ -113,7 +120,8 @@ the image color count (256 by default)."))
          (setf (slot-value image 'pixels) pixels))
         ((and (numberp width) (numberp height))
          (setf (slot-value image 'pixels)
-               (make-array (list height width) :initial-element 0)))
+               (make-array (list height width)
+                           :element-type 'indexed-pixel)))
         (t (error "Invalid initialization arguments"))))
 
 (defmethod pixel-size ((image indexed-image)) 1)
