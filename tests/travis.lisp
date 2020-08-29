df1a390bbfcbc757a7c5dbe3b0ee0b3acbac6c00
Vasily Postnicov <shamaz.mazum@gmail.com>
2020-08-29 12:50:40 +0300

    Test imago/jpeg-turbo

--- tests/travis.lisp
+++ tests/travis.lisp
@@ -1,4 +1,6 @@
 (defun do-all()
+  (if (string= "YES" (uiop:getenv "WITH_JPEG_TURBO"))
+      (ql:quickload :imago/jpeg-turbo))
   (ql:quickload :imago/tests)
   (uiop:quit
    (if (uiop:call-function "imago-tests:run-tests")
