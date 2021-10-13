29f2b42b248785acae3d05d5dd97a4e9ad0d8ecb
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-10-13 10:47:08 +0300

    show-image shows binary images in Jupyter

--- jupyter/jupyter.lisp
+++ jupyter/jupyter.lisp
@@ -1,8 +1,19 @@
 (in-package :imago-jupyter)
 
-(defun show-image (image)
-  (declare (type image image))
+(defgeneric show-image (image)
+  (:documentation "Show an image in Jupyter notebook"))
+
+;; KLUDGE: since binary images cannot be saved, convert them to
+;; grayscale first.
+;; Maybe write saver functions to binary images? But since jpeg is
+;; lossy format, such images will cease to be binary after
+;; saving. This is tricky...
+
+(defmethod show-image ((image image))
   (jpeg
    (usb8-array-to-base64-string
     (with-output-to-sequence (stream)
       (write-jpg-stream image stream)))))
+
+(defmethod show-image ((image binary-image))
+  (show-image (convert-to-grayscale image)))
