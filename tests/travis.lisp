ce74bd70915aa8941f648e1cdb1607cfcb78f350
Vasily Postnicov <shamaz.mazum@gmail.com>
2026-02-08 17:25:15 +0300

    Use qlot

--- tests/travis.lisp
+++ tests/travis.lisp
@@ -1,10 +1,15 @@
 (defun do-all()
-  (when (string= "YES" (uiop:getenv "WITH_ALTIO"))
-    (ql:quickload :imago/jpeg-turbo)
-    (ql:quickload :imago/pngload))
-  (ql:quickload :imago/tests)
+  (handler-case
+      (progn
+        (when (string= "YES" (uiop:getenv "WITH_ALTIO"))
+          (asdf:load-system :imago/jpeg-turbo)
+          (asdf:load-system :imago/libheif)
+          (asdf:load-system :imago/pngload))
+        (asdf:load-system :imago/tests))
+    (error ()
+      (uiop:quit 1)))
   (uiop:quit
    (if (uiop:call-function "imago-tests:run-tests")
-        0 1)))
+       0 1)))
 
 (do-all)
